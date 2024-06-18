571cdcd868b3db322398d78cd562c2f215090544
Ghanshyam Gupta <ghanshyamg.cs.22@nitj.ac.in>
2024-06-19 00:29:25 +0530

    first commit

---
+++ src/pages/details/carousels/Recommendation.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Carousel from "../../../components/carousel/Carousel";
+import useFetch from "../../../hooks/useFetch";
+
+const Recommendation = ({ mediaType, id }) => {
+   return <div className="">Recommendation</div>
+};
+
+export default Recommendation;
---
+++ src/pages/details/carousels/Similar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Carousel from "../../../components/carousel/Carousel";
+import useFetch from "../../../hooks/useFetch";
+
+const Similar = ({ mediaType, id }) => {
+    return <div className="">Similiar</div>
+};
+
+export default Similar;
